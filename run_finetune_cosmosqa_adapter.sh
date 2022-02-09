f11ed6faedb82e564d1e791e5b177842c58942fb
fnk93 <f.krueger1993@gmail.com>
2022-02-09 23:59:56 +0100

    fix script

--- run_finetune_cosmosqa_adapter.sh
+++ run_finetune_cosmosqa_adapter.sh
@@ -13,29 +13,29 @@ lin_adap='./pretrained_models/lin-adapter/pytorch_model.bin'
 comment='fl-adapter'
 dataset=data/cosmosQA
 CUDA_VISIBLE_DEVICES=$GPU python examples/run_finetune_cosmosqa_adapter.py \
---model_type roberta-large \
---model_name_or_path roberta-large \
---do_train \
---comment $comment \
---do_eval \
---data_dir $dataset \
---preprocess_type read_examples_origin \
---output_dir ./proc_data/roberta_cosmosqa \
---max_seq_length $seq_length \
---eval_steps 200 \
---per_gpu_train_batch_size $batch \
---gradient_accumulation_steps $accu \
---warmup_steps $warmup \
---per_gpu_eval_batch_size $batch \
---learning_rate $lr \
---adam_epsilon 1e-6 \
---weight_decay 0 \
---train_steps 20000 \
---report_steps 20000000000 \
---freeze_bert="" \
---freeze_adapter="True" \
---adapter_size 768 \
---adapter_list "0,11,22" \
---adapter_skip_layers 0 \
---meta_fac_adaptermodel=$fac_adap \
---meta_lin_adaptermodel=$lin_adap
+    --model_type roberta-large \
+    --model_name_or_path roberta-large \
+    --do_train \
+    --comment $comment \
+    --do_eval \
+    --data_dir $dataset \
+    --preprocess_type read_examples_origin \
+    --output_dir ./proc_data \
+    --max_seq_length $seq_length \
+    --eval_steps 200 \
+    --per_gpu_train_batch_size $batch \
+    --gradient_accumulation_steps $accu \
+    --warmup_steps $warmup \
+    --per_gpu_eval_batch_size $batch \
+    --learning_rate $lr \
+    --adam_epsilon 1e-6 \
+    --weight_decay 0 \
+    --train_steps 20000 \
+    --report_steps 20000000000 \
+    --freeze_bert="" \
+    --freeze_adapter="True" \
+    --adapter_size 768 \
+    --adapter_list "0,11,22" \
+    --adapter_skip_layers 0 \
+    --meta_fac_adaptermodel=$fac_adap \
+    --meta_lin_adaptermodel=$lin_adap
