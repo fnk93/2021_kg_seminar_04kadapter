68a1a768d0ca9cee06a907f1549d7f1e8ae17f51
fnk93 <f.krueger1993@gmail.com>
2022-02-09 19:18:27 +0100

    small updates

---
+++ get_fb15k_237.sh
@@ -0,0 +1,3 @@
+cd data
+wget https://github.com/villmow/datasets_knowledge_embedding/raw/master/FB15k-237/original/FB15k-237.tar.xz
+tar -zxvf FB15k-237.tar.xz
---
+++ get_figer.sh
@@ -0,0 +1,3 @@
+cd data
+gdown https://drive.google.com/file/d/0B52yRXcdpG6MMnRNV3dTdGdYQ2M -O - --quiet | tar zxvf -
+tar -zxvf ultrafine_acl18.tar.gz
