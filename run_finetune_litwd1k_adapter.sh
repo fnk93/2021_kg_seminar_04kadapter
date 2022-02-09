0df6badc69b5e4c2925a1737876fdbaa63c3c78d
fnk93 <f.krueger1993@gmail.com>
2022-02-09 18:28:35 +0100

    use best params as for figer

--- run_finetune_litwd1k_adapter.sh
+++ run_finetune_litwd1k_adapter.sh
@@ -2,10 +2,30 @@
 # Licensed under the MIT license.
 
 task=entity_type_kg
-warmup=120
-learning=3e-5
-batch_size=4
+# # best results on F
+# seq_length=256
+# batch_size=2048
+# lr=5e-6
+# warmup=500
+# fac_adap='./pretrained_models/fac-adapter/pytorch_model.bin'
+# lin_adap=''
+# comment='f-litwd1k-adapter'
+# # best results on L
+# seq_length=256
+# batch_size=2048
+# lr=5e-6
+# warmup=200
+# fac_adap=''
+# lin_adap='./pretrained_models/lin-adapter/pytorch_model.bin'
+# comment='l-litwd1k-adapter'
+# best results on F + L
 seq_length=256
+batch_size=2048
+lr=5e-6
+warmup=1000
+fac_adap='./pretrained_models/fac-adapter/pytorch_model.bin'
+lin_adap='./pretrained_models/lin-adapter/pytorch_model.bin'
+comment='fl-litwd1k-adapter'
 dataset=data/LitWD1K
 python examples/run_finetune_litWik_adapter.py \
     --model_type roberta \
@@ -17,7 +37,7 @@ python examples/run_finetune_litWik_adapter.py \
     --task_name=$task     \
     --data_dir=$dataset  \
     --output_dir=./proc_data \
-    --comment 'figer-adapter' \
+    --comment $comment \
     --max_seq_length=$seq_length  \
     --per_gpu_eval_batch_size=$batch_size   \
     --per_gpu_train_batch_size=$batch_size   \
@@ -35,6 +55,5 @@ python examples/run_finetune_litWik_adapter.py \
     --adapter_list "0,11,22" \
     --adapter_skip_layers 0 \
     --task_adapter '' \
-    --meta_fac_adaptermodel="./pretrained_models/fac-adapter/pytorch_model.bin" \
-    --meta_lin_adaptermodel="./pretrained_models/lin-adapter/pytorch_model.bin" \
-    --restore
+    --meta_fac_adaptermodel=$fac_adap \
+    --meta_lin_adaptermodel=$lin_adap \
