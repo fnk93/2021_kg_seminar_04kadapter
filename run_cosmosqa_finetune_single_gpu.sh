4663be8ba36657399198f4a16fcdf194e44b2ac7
fnk93 <f.krueger1993@gmail.com>
2022-02-13 14:48:43 +0100

    add cosmosQA finetuning for all adapters

--- run_cosmosqa_finetune_single_gpu.sh
+++ run_cosmosqa_finetune_single_gpu.sh
@@ -44,3 +44,73 @@ python examples/run_finetune_cosmosqa_adapter.py \
     --meta_lin_adaptermodel=$lin_adap \
     --save_to_s3 \
     --read_from_s3
+
+fac_adap="./pretrained_models/fac-adapter/pytorch_model.bin"
+lin_adap=""
+comment="f-adapter"
+CUDA_VISIBLE_DEVICES=$GPU
+python examples/run_finetune_cosmosqa_adapter.py \
+    --model_type roberta-large \
+    --model_name_or_path roberta-large \
+    --do_train \
+    --comment $comment \
+    --do_eval \
+    --data_dir $dataset \
+    --preprocess_type read_examples_origin \
+    --output_dir ./proc_data \
+    --max_seq_length $seq_length \
+    --eval_steps 200 \
+    --per_gpu_train_batch_size $batch \
+    --gradient_accumulation_steps $accu \
+    --warmup_steps $warmup \
+    --per_gpu_eval_batch_size $batch \
+    --learning_rate $lr \
+    --save_steps=2000 \
+    --adam_epsilon 1e-6 \
+    --weight_decay 0 \
+    --train_steps 20000 \
+    --report_steps 20000000000 \
+    --freeze_bert="" \
+    --freeze_adapter="True" \
+    --adapter_size 768 \
+    --adapter_list "0,11,22" \
+    --adapter_skip_layers 0 \
+    --meta_fac_adaptermodel=$fac_adap \
+    --meta_lin_adaptermodel=$lin_adap \
+    --save_to_s3 \
+    --read_from_s3
+
+fac_adap=""
+lin_adap="./pretrained_models/lin-adapter/pytorch_model.bin"
+comment="l-adapter"
+CUDA_VISIBLE_DEVICES=$GPU
+python examples/run_finetune_cosmosqa_adapter.py \
+    --model_type roberta-large \
+    --model_name_or_path roberta-large \
+    --do_train \
+    --comment $comment \
+    --do_eval \
+    --data_dir $dataset \
+    --preprocess_type read_examples_origin \
+    --output_dir ./proc_data \
+    --max_seq_length $seq_length \
+    --eval_steps 200 \
+    --per_gpu_train_batch_size $batch \
+    --gradient_accumulation_steps $accu \
+    --warmup_steps $warmup \
+    --per_gpu_eval_batch_size $batch \
+    --learning_rate $lr \
+    --save_steps=2000 \
+    --adam_epsilon 1e-6 \
+    --weight_decay 0 \
+    --train_steps 20000 \
+    --report_steps 20000000000 \
+    --freeze_bert="" \
+    --freeze_adapter="True" \
+    --adapter_size 768 \
+    --adapter_list "0,11,22" \
+    --adapter_skip_layers 0 \
+    --meta_fac_adaptermodel=$fac_adap \
+    --meta_lin_adaptermodel=$lin_adap \
+    --save_to_s3 \
+    --read_from_s3
