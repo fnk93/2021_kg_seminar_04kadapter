fb3543940f3a0dba6b14abb3ef41bae5bca904cb
fnk93 <f.krueger1993@gmail.com>
2022-02-16 10:57:55 +0100

    add original k-adapter implementation

---
+++ run_cosmosqa_finetune_single_gpu_bak_bak.sh
@@ -0,0 +1,110 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+# best results for F + L
+batch=8
+# batch=8
+accu=1
+lr=1e-5
+GPU="0"
+warmup=0
+seq_length=256
+fac_adap="./pretrained_models/fac-adapter/pytorch_model.bin"
+lin_adap="./pretrained_models/lin-adapter/pytorch_model.bin"
+comment="fl-adapter"
+dataset=data/cosmosQA
+CUDA_VISIBLE_DEVICES=$GPU
+python examples/run_finetune_cosmosqa_adapter_bak_bak.py \
+    --model_type roberta-large \
+    --model_name_or_path roberta-large \
+    --do_train \
+    --comment $comment \
+    --do_eval \
+    --data_dir $dataset \
+    --preprocess_type read_examples_origin \
+    --output_dir ./proc_data \
+    --max_seq_length $seq_length \
+    --eval_steps 200 \
+    --per_gpu_train_batch_size $batch \
+    --gradient_accumulation_steps $accu \
+    --warmup_steps $warmup \
+    --per_gpu_eval_batch_size $batch \
+    --learning_rate $lr \
+    --save_steps=2000 \
+    --adam_epsilon 1e-6 \
+    --weight_decay 0 \
+    --train_steps 20000 \
+    --report_steps 20000000000 \
+    --freeze_bert="" \
+    --freeze_adapter="True" \
+    --adapter_size 768 \
+    --adapter_list "0,11,22" \
+    --adapter_skip_layers 0 \
+    --meta_fac_adaptermodel=$fac_adap \
+    --meta_lin_adaptermodel=$lin_adap
+
+fac_adap="./pretrained_models/fac-adapter/pytorch_model.bin"
+lin_adap=""
+comment="f-adapter"
+CUDA_VISIBLE_DEVICES=$GPU
+python examples/run_finetune_cosmosqa_adapter_bak.py \
+    --model_type roberta-large \
+    --model_name_or_path roberta-large \
+    --do_train \
+    --comment $comment \
+    --do_eval \
+    --data_dir $dataset \
+    --preprocess_type read_examples_origin \
+    --output_dir ./proc_data \
+    --max_seq_length $seq_length \
+    --eval_steps 200 \
+    --per_gpu_train_batch_size $batch \
+    --gradient_accumulation_steps $accu \
+    --warmup_steps $warmup \
+    --per_gpu_eval_batch_size $batch \
+    --learning_rate $lr \
+    --save_steps=2000 \
+    --adam_epsilon 1e-6 \
+    --weight_decay 0 \
+    --train_steps 20000 \
+    --report_steps 20000000000 \
+    --freeze_bert="" \
+    --freeze_adapter="True" \
+    --adapter_size 768 \
+    --adapter_list "0,11,22" \
+    --adapter_skip_layers 0 \
+    --meta_fac_adaptermodel=$fac_adap \
+    --meta_lin_adaptermodel=$lin_adap
+
+fac_adap=""
+lin_adap="./pretrained_models/lin-adapter/pytorch_model.bin"
+comment="l-adapter"
+CUDA_VISIBLE_DEVICES=$GPU
+python examples/run_finetune_cosmosqa_adapter_bak.py \
+    --model_type roberta-large \
+    --model_name_or_path roberta-large \
+    --do_train \
+    --comment $comment \
+    --do_eval \
+    --data_dir $dataset \
+    --preprocess_type read_examples_origin \
+    --output_dir ./proc_data \
+    --max_seq_length $seq_length \
+    --eval_steps 200 \
+    --per_gpu_train_batch_size $batch \
+    --gradient_accumulation_steps $accu \
+    --warmup_steps $warmup \
+    --per_gpu_eval_batch_size $batch \
+    --learning_rate $lr \
+    --save_steps=2000 \
+    --adam_epsilon 1e-6 \
+    --weight_decay 0 \
+    --train_steps 20000 \
+    --report_steps 20000000000 \
+    --freeze_bert="" \
+    --freeze_adapter="True" \
+    --adapter_size 768 \
+    --adapter_list "0,11,22" \
+    --adapter_skip_layers 0 \
+    --meta_fac_adaptermodel=$fac_adap \
+    --meta_lin_adaptermodel=$lin_adap
